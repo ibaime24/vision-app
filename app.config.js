638b3c3338be2a36a23a561c5dc8ff1dd5a3091f
Ian Baime <ibaime24@students.claremontmckenna.edu>
2025-10-12 15:15:58 -0700

    dependencies upgraded

--- app.config.js
+++ app.config.js
@@ -5,6 +5,22 @@ export default {
     name: "VisionAssistant",
     slug: "VisionAssistant",
     version: "1.0.0",
+    ios: {
+      bundleIdentifier: "com.anonymous.VisionAssistant",
+      supportsTablet: true
+    },
+    android: {
+      package: "com.anonymous.VisionAssistant",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    plugins: [
+      "expo-font",
+      "expo-router",
+      "expo-web-browser"
+    ],
     extra: {
       EXPO_PUBLIC_OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY || null,
       EXPO_PUBLIC_ELEVEN_LABS_API_KEY: process.env.EXPO_PUBLIC_ELEVEN_LABS_API_KEY || null,
